13e2f41ce14238d33bac16657c1a2a2a1efef69e
Neil Mitchell <ndmitchell@gmail.com>
2016-11-28 09:11:26 +0000

    Add -O3

--- go.bat
+++ go.bat
@@ -1,2 +1,2 @@
 rem sos -p .*.c -c "go 2>&1 | head -n12"
-gcc -c hexml.c && gcc -c main.c && gcc -o hexml hexml.o main.o && hexml
+gcc -c hexml.c -O3 && gcc -c main.c -O3 && gcc -o hexml hexml.o main.o && hexml
