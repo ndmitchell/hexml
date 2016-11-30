b379e7769fbbefdc108647aa362b4e2af2f6637a
Neil Mitchell <ndmitchell@gmail.com>
2016-11-30 19:08:42 +0000

    Add proper visual studio debugging support

--- go.bat
+++
@@ -1,2 +0,0 @@
-rem sos -p .*.c -c "go 2>&1 | head -n12"
-gcc -c hexml.c -O3 && gcc -c main.c -O3 && gcc -o hexml hexml.o main.o && hexml
---
+++ vs/main.c
@@ -0,0 +1,40 @@
+#include "hexml.h"
+#include <stdio.h>
+#include <malloc.h>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+
+char* readFile(char* file)
+{
+    FILE* f = fopen(file, "rb");
+    fseek(f, 0, SEEK_END);
+    int len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    char* res = malloc(len + 1);
+    res[len] = 0;
+    int n = fread(res, 1, len, f);
+    assert(n == len);
+    fclose(f);
+    return res;
+}
+
+char* example = "<?xml version=\"1.0\" lang=\"ja\"?><foo><bar baz=\"qux\">quux</bar><bar baz=\"bar\">hoge</bar><piyo>&gt;piyopiyo&lt;</piyo></foo><foo2 />";
+
+void main(int argc, char **argv)
+{
+    setbuf(stdout, NULL);
+    char* body = argv[1] == NULL ? example : readFile(argv[1]);
+    document* doc = document_parse(body, -1);
+    char* err = document_error(doc);
+    if (err == NULL)
+    {
+        int len = node_render(doc, document_node(doc), NULL, 0);
+        char* s = malloc(len + 1);
+        node_render(doc, document_node(doc), s, len);
+        s[len] = 0;
+        printf("Parse successful\n"); // , %s\n", s);
+    }
+    else
+        printf("Parse failed, %s\n", err);
+}
