591c8238df9807b35ae39ff010016a60215b00ce
Neil Mitchell <ndmitchell@gmail.com>
2017-07-08 17:05:20 +0100

    Turn on all possible warnings when debugging hexml.c

--- ghci.bat
+++ ghci.bat
@@ -1,1 +1,1 @@
-gcc -O2 -std=c99 -c cbits\hexml.c -o cbits\hexml.o && ghci.exe cbits\hexml.o %*
+gcc -O2 -std=c99 -c cbits\hexml.c -o cbits\hexml.o -Wall -Wextra -Werror && ghci.exe cbits\hexml.o %*
