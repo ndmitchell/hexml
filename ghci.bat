10b4ba47fb4a29b5de5e10aa22211d16272079a3
Neil Mitchell <ndmitchell@gmail.com>
2018-01-04 23:09:22 +0000

    Given a clue as to how to do the new code in ghci

--- ghci.bat
+++ ghci.bat
@@ -1,1 +1,2 @@
 gcc -O2 -std=c99 -c cbits\hexml.c -o cbits\hexml.o -Wall -Wextra -Werror && ghci.exe cbits\hexml.o %*
+rem gcc -O2 -std=c99 -c gen\test.c -o gen\test.o -Wall -Wextra -Werror && ghci.exe gen\test.o %*
